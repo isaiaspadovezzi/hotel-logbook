d9faeaf93e6fea71bced416fb141fa20e43cd020
isaiaspadovezzi <padovezzi@gmail.com>
2026-08-10 16:04:37 -0300

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -127,7 +127,7 @@ fillColor:[99,193,50],
 // LOGO COM PROPORÇÃO ORIGINAL
 // ------------------------------------
 
-const larguraLogo = 10;
+const larguraLogo = 20;
 
 const alturaLogo =
     larguraLogo * logo.height / logo.width;

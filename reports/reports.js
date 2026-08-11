533c0464b214c49919ed499ddd0af1ec7285e747
isaiaspadovezzi <padovezzi@gmail.com>
2026-08-10 22:16:47 -0300

    Update reports.js

--- reports/reports.js
+++ reports/reports.js
@@ -1,1 +1,10 @@
 reports/reports.js
+// =====================================================
+// LOGBOOK - REPORTS
+// Sistema de notificações
+// =====================================================
+
+// Arquivo reservado para a função de Reportar
+// Não altera o funcionamento do LogBook principal.
+
+console.log("Módulo Reports carregado.");
